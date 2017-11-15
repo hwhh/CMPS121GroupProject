3fc37c3778113dca67fb1622048838fe1bc6d9b9
Hailey Pun <hei.pun@kcl.ac.uk>
2017-11-14 16:34:24 -0800

    Generate buttons programmatically

--- app/src/main/java/com/groupproject/Controller/PrivateFragment.java
+++ app/src/main/java/com/groupproject/Controller/PrivateFragment.java
@@ -1,10 +1,13 @@
 package com.groupproject.Controller;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.groupproject.R;
 
@@ -14,10 +17,29 @@ import com.groupproject.R;
 
 public class PrivateFragment extends Fragment {
 
+    ImageButton[] eventButton = new ImageButton[3];
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.private_fragment, container, false);
+
+        LinearLayout buttonPanel = (LinearLayout) rootView.findViewById(R.id.buttonPanel);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        for (int i = 0; i < 3; i++ ){
+
+            eventButton[i] = new ImageButton(getContext());
+            ImageButton button = eventButton[i];
+            button.setImageResource(R.drawable.eventpic);
+            button.setLayoutParams(lp);
+            button.setBackgroundColor(Color.TRANSPARENT);
+            button.setPadding(15, 15, 15, 15 );
+            button.setTag(i);
+            button.setId(i);
+            buttonPanel.addView(eventButton[i]);
+
+        }
 
         return rootView;
     }
--- app/src/main/java/com/groupproject/Controller/PublicFragment.java
+++ app/src/main/java/com/groupproject/Controller/PublicFragment.java
@@ -1,5 +1,6 @@
 package com.groupproject.Controller;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.groupproject.R;
 
@@ -20,12 +23,30 @@ public class PublicFragment extends Fragment {
 //    RecyclerView.LayoutManager mLayoutManager;
     RecyclerView.Adapter mAdapter;
 
+    ImageButton[] eventButton = new ImageButton[3];
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.public_fragment, container, false);
 
+        LinearLayout buttonPanel = (LinearLayout) rootView.findViewById(R.id.buttonPanel);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        for (int i = 0; i < 3; i++ ){
+
+            eventButton[i] = new ImageButton(getContext());
+            ImageButton button = eventButton[i];
+            button.setImageResource(R.drawable.pinnedbutton);
+            button.setLayoutParams(lp);
+            button.setBackgroundColor(Color.TRANSPARENT);
+            button.setPadding(15, 15, 15, 15 );
+            button.setTag(i);
+            button.setId(i);
+            buttonPanel.addView(eventButton[i]);
+
+        }
 //        // Calling the RecyclerView
 //        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
 //        mRecyclerView.setHasFixedSize(true);
