53ecdd8372d1a19067150d1a2772cb6107faa25e
Mattheo <mattheoioannou@gmail.com>
2017-11-22 00:41:06 -0800

    Fixed location issues, added progressbar when location can't be found

--- app/src/main/java/com/groupproject/Controller/MapsFragment.java
+++ app/src/main/java/com/groupproject/Controller/MapsFragment.java
@@ -1,7 +1,6 @@
 package com.groupproject.Controller;
 
 import android.Manifest;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -15,8 +14,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -24,13 +27,13 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.groupproject.DataBaseAPI.DataBaseAPI;
 import com.groupproject.Model.Event;
-import com.groupproject.Model.LocationHelper;
-import com.groupproject.Model.LocationHelper.LocationRes;
 import com.groupproject.R;
 
 import java.util.Calendar;
@@ -40,22 +43,11 @@ public class MapsFragment extends Fragment {
 
 
     private static final int DEFAULT_ZOOM = 15;
-    LocationHelper locHelper;
+    private boolean foundLocation;
     MapView mMapView;
     private GoogleMap googleMap;
     private DataBaseAPI dataBaseAPI = DataBaseAPI.getDataBase();
     private LocationControl locationControlTask;
-    private boolean hasLocation = false;
-    private Location currentLocation;
-    public LocationRes locationResult = new LocationRes() {
-
-        @Override
-        public void gotLocation(Location location) {
-            currentLocation = new Location(location);
-            hasLocation = true;
-        }
-
-    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -87,16 +79,7 @@ public class MapsFragment extends Fragment {
                     }
                 });
 
-                if (ActivityCompat.checkSelfPermission(
-                        getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) !=
-                        PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) !=
-                        PackageManager.PERMISSION_GRANTED) {
-
-                    ActivityCompat.requestPermissions(getActivity(),
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                                    Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-                }
+                askForLocationPermissions();
             }
 
         });
@@ -122,18 +105,10 @@ public class MapsFragment extends Fragment {
                 });
             }
         });
-
-        Context context = this.getContext();
-
-        locHelper = new LocationHelper();
-        locHelper.getLocation(context, locationResult);
-        locationControlTask = new LocationControl();
-        locationControlTask.execute(this.getActivity());
-
         setUpListener();
         return rootView;
     }
-//
+
     private void setUpListener() {
         ChildEventListener childEventListener = new ChildEventListener() {
             @Override
@@ -171,8 +146,8 @@ public class MapsFragment extends Fragment {
     @Override
     public void onStop() {
         super.onStop();
-        locHelper.stopLocationUpdates();
-        locationControlTask.cancel(true);
+        if (locationControlTask != null)
+            locationControlTask.cancel(true);
     }
 
     public void addPinsToMap() {
@@ -184,14 +159,6 @@ public class MapsFragment extends Fragment {
                     .snippet(event.getEndDate().toString()));
         }
     }
-
-    private void useLocation() {
-        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                latLng, DEFAULT_ZOOM));
-    }
-
-
 
     @Override
     public void onResume() {
@@ -218,12 +185,13 @@ public class MapsFragment extends Fragment {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
-        //TODO: Error message if location denied?
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
         switch (requestCode) {
             case 1: {
-                if ((grantResults.length > 0) && (grantResults[0] + grantResults[1]) == PackageManager.PERMISSION_GRANTED) {
-                    //TODO EMPTY IF STATEMENT
+                if ((grantResults.length > 0) &&
+                        (grantResults[0] + grantResults[1]) == PackageManager.PERMISSION_GRANTED) {
+                    setCurrentLocation();
                 } else {
                     ActivityCompat.requestPermissions(getActivity(),
                             new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
@@ -233,18 +201,33 @@ public class MapsFragment extends Fragment {
         }
     }
 
+    private void askForLocationPermissions() {
+        if (ActivityCompat.checkSelfPermission(
+                getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+            MapsFragment.this.requestPermissions(
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        } else {
+            setCurrentLocation();
+        }
+    }
+
     private class LocationControl extends AsyncTask<Context, Void, Void> {
-        private final ProgressDialog dialog = new ProgressDialog(getActivity());
+        ProgressBar progressBar = (ProgressBar) getView().findViewById(R.id.progressBar);
 
         protected void onPreExecute() {
-            this.dialog.setMessage("Searching");
-            this.dialog.show();
+            progressBar.setVisibility(View.VISIBLE);
         }
 
         protected Void doInBackground(Context... params) {
-            //Wait 10 seconds to see if we can get a location from either network or GPS, otherwise stop
+            //Wait 15 seconds to see if we can get a location from either network or GPSp
             Long t = Calendar.getInstance().getTimeInMillis();
-            while (!hasLocation && Calendar.getInstance().getTimeInMillis() - t < 15000) {
+            while (!foundLocation) {
+                searchForLocation();
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -255,19 +238,71 @@ public class MapsFragment extends Fragment {
         }
 
         protected void onPostExecute(final Void unused) {
-            if (this.dialog.isShowing()) {
-                this.dialog.dismiss();
-            }
-
-            if (currentLocation != null) {
-                useLocation();
-            } else {
-                //Couldn't find location, do something like show an alert dialog
-            }
+            progressBar.setVisibility(View.GONE);
+            setCurrentLocation();
         }
     }
 
+    /**
+     * Search for location without creating an async task
+     */
+    private void searchForLocation() {
+        foundLocation = false;
+        FusedLocationProviderClient mFusedLocationProviderClient = LocationServices
+                .getFusedLocationProviderClient(getActivity());
+        //Ignore the red line - we check for permissions before this function is called
+        Task<Location> lastLocation = mFusedLocationProviderClient.getLastLocation();
+        lastLocation.addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    // Set the map's camera position to the current location of the device.
+                    Location location = task.getResult();
+                    if (location != null)  {
+                        LatLng currentLatLng = new LatLng(location.getLatitude(),
+                                location.getLongitude());
+                        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(currentLatLng,
+                                DEFAULT_ZOOM);
+                        googleMap.moveCamera(update);
+                        foundLocation = true;
+                    }
+                }
+            }
+        });
+    }
 
+    /**
+     * Set current location, creating an async task if the location can't be found at that moment
+     */
+    private void setCurrentLocation() {
+        foundLocation = false;
+        //Ignore the red line - we check for permissions before this function is called
+        googleMap.setMyLocationEnabled(true);
+        FusedLocationProviderClient mFusedLocationProviderClient = LocationServices
+                .getFusedLocationProviderClient(getActivity());
+        //Ignore the red line - we check for permissions before this function is called
+        Task<Location> lastLocation = mFusedLocationProviderClient.getLastLocation();
+        lastLocation.addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    // Set the map's camera position to the current location of the device.
+                    Location location = task.getResult();
+                    if (location == null) {
+                        locationControlTask = new LocationControl();
+                        locationControlTask.execute(getActivity());
+                    } else {
+                        LatLng currentLatLng = new LatLng(location.getLatitude(),
+                                location.getLongitude());
+                        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(currentLatLng,
+                                DEFAULT_ZOOM);
+                        googleMap.moveCamera(update);
+                        foundLocation = true;
+                    }
+                }
+            }
+        });
+    }
 }
 
 
