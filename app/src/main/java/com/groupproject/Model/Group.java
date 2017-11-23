1910badfc02c61f2a9115d7197610bf614d4bb09
hwhh <hwhh@st-andrews.ac.uk>
2017-11-23 12:06:11 -0800

    updated the contributions

--- app/src/main/java/com/groupproject/Model/Group.java
+++ app/src/main/java/com/groupproject/Model/Group.java
@@ -1,5 +1,6 @@
 package com.groupproject.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -11,13 +12,52 @@ public class Group {
     private List<Event> events;
     private List<String > eventsIDs;
 
+    private List<String > tags;
+
     private String name;
     private String description;
 
     private Event.VISIBILITY visibility;
 
+    public Group() {
+    }
+
     public Group(String name, String description) {
         this.name = name;
         this.description = description;
+        init();
+    }
+
+    private void init(){
+        memebrs = new ArrayList<>();
+        memebrsIDs = new ArrayList<>();
+        events= new ArrayList<>();
+        eventsIDs= new ArrayList<>();
+        tags= new ArrayList<>();
+    }
+
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public List<String> getMemebrsIDs() {
+        return memebrsIDs;
+    }
+
+    public List<String> getEventsIDs() {
+        return eventsIDs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Event.VISIBILITY getVisibility() {
+        return visibility;
     }
 }
