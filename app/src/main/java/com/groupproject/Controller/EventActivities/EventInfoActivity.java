99bbbbaa3af2e654063645f81a09413c2c0daf8f
hwhh <hwhh@st-andrews.ac.uk>
2017-12-06 01:23:01 -0800

    added the inviting

--- app/src/main/java/com/groupproject/Controller/EventActivities/EventInfoActivity.java
+++ app/src/main/java/com/groupproject/Controller/EventActivities/EventInfoActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.Query;
 import com.groupproject.Controller.InviteActivity;
 import com.groupproject.Controller.SearchActivities.SearchType;
 import com.groupproject.Controller.ViewHolder;
@@ -20,12 +24,14 @@ import com.groupproject.Model.User;
 import com.groupproject.R;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 public class EventInfoActivity extends AppCompatActivity implements DataBaseCallBacks<String> {
 
-    private static final DataBaseAPI database = DataBaseAPI.getDataBase();
+    private static final DataBaseAPI dataBaseAPI = DataBaseAPI.getDataBase();
     private TextView eventText;
     private TextView startDateText;
     private TextView endDateText;
@@ -35,6 +41,9 @@ public class EventInfoActivity extends AppCompatActivity implements DataBaseCall
     private Button joinButton;
     private Event event;
     private User user;
+    List<String> goingEventsLists = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,14 +56,14 @@ public class EventInfoActivity extends AppCompatActivity implements DataBaseCall
         numOfPeopleText = findViewById(R.id.numOfPeopleText);
         joinButton = findViewById(R.id.btn_join);
         resetEvent();
-        database.getUser(database.getCurrentUserID(), this, null);
+        dataBaseAPI.getUser(dataBaseAPI.getCurrentUserID(), this, null);
 
         joinButton.setOnClickListener(v -> {
             if (event != null && user != null) {
                 if (userIsHost()) {
                     DialogInterface.OnClickListener dialogClickListener = (dialog, which) -> {
                         if (which == DialogInterface.BUTTON_POSITIVE) {
-                            database.deleteEvent(event);
+                            dataBaseAPI.deleteEvent(event);
                             finish();
                         }
                     };
@@ -64,11 +73,11 @@ public class EventInfoActivity extends AppCompatActivity implements DataBaseCall
                             .setNegativeButton("No", dialogClickListener).show();
                 } else {
                     if (userGoingToEvent()) {
-                        database.leaveEvent(event);
+                        dataBaseAPI.leaveEvent(event);
                         resetEvent();
                         switchButton();
                     } else {
-                        database.acceptEventInvite(event);
+                        dataBaseAPI.acceptEventInvite(event);
                         resetEvent();
                         switchButton();
                     }
@@ -91,7 +100,7 @@ public class EventInfoActivity extends AppCompatActivity implements DataBaseCall
         event = null;
         if (getIntent().hasExtra("key")) {
             String event_id = getIntent().getStringExtra("key");
-            database.getEvent(event_id, this, null);
+            dataBaseAPI.getEvent(event_id, this, null);
         }
     }
 
@@ -101,11 +110,11 @@ public class EventInfoActivity extends AppCompatActivity implements DataBaseCall
     }
 
     private boolean userGoingToEvent() {
-        return event.goingIDs != null && event.goingIDs.contains(database.getCurrentUserID());
+        return event.goingIDs != null && event.goingIDs.contains(dataBaseAPI.getCurrentUserID());
     }
 
     private boolean userIsHost() {
-        return event != null && event.getHostID().equals(database.getCurrentUserID());
+        return event != null && event.getHostID().equals(dataBaseAPI.getCurrentUserID());
     }
 
     private void assignButton() {
@@ -156,6 +165,9 @@ public class EventInfoActivity extends AppCompatActivity implements DataBaseCall
     public void getUser(User user, ViewHolder holder) {
         this.user = user;
         assignButton();
+        goingEventsLists.add(user.getName());
+        adapter.notifyDataSetChanged();
+
     }
 
     @Override
@@ -163,6 +175,13 @@ public class EventInfoActivity extends AppCompatActivity implements DataBaseCall
         this.event = event;
         display(event);
         assignButton();
+        ListView goingEvents = findViewById( R.id.goingUsers);
+        adapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_spinner_item, goingEventsLists);
+        goingEvents.setAdapter(adapter);
+        Query query = dataBaseAPI.getmEventRef().child(event.getId()).child("goingIDs");
+        dataBaseAPI.executeQuery(query, this, SearchType.Type.EVENTS);
+
     }
 
     @Override
@@ -172,7 +191,9 @@ public class EventInfoActivity extends AppCompatActivity implements DataBaseCall
 
     @Override
     public void executeQuery(List<String> result, SearchType.Type type) {
-
+        for (String id : result) {
+            dataBaseAPI.getUser(id, this, null);
+        }
     }
 
 
