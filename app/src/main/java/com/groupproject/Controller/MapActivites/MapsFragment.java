4d6edbfda1e408341f4ba0b3530076e0d620d0f5
Hailey Pun <hei.pun@kcl.ac.uk>
2017-12-06 23:51:18 -0800

    Fixed page

--- app/src/main/java/com/groupproject/Controller/MapActivites/MapsFragment.java
+++ app/src/main/java/com/groupproject/Controller/MapActivites/MapsFragment.java
@@ -110,7 +110,10 @@ public class MapsFragment extends Fragment implements GoogleMap.OnMarkerClickLis
         ChildEventListener childEventListener = new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-
+                Event event = dataSnapshot.getValue(com.groupproject.Model.Event.class);
+                if (event != null) {
+                    addPinsToMap();
+                }
             }
 
 
